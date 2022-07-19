717c9a291f19cb55d3ef94744f64fe86c2275bec
Daniela Escudero <87047514+DanielaSEscudero@users.noreply.github.com>
2022-07-19 12:16:01 -0300

    sin login

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './inicio.css'
 
-import Login from '../Login/Login'
+//import Login from '../Login/Login'
 
 
 const Inicio = () => {
@@ -11,9 +11,9 @@ const Inicio = () => {
         <h1 className="saludo text-center">Bienvenidos</h1>
         <img src="https://firebasestorage.googleapis.com/v0/b/tienda-f9328.appspot.com/o/Logo%20emprendimiento%20manuscrito%20rosa.png?alt=media&token=23703646-a42d-4568-9b1a-f5d78720caa4" alt="" />
       </div>
-      <div className="col-md-6">
+    {/*<div className="col-md-6">
         < Login />
-      </div>
+      </div>*/}
     </div>
   )
 }
